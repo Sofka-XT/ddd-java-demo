a9c75ff13850b072cf0fa57e791f186e11b31449
Lisbey Urrea <lisbey.urrea@sofka.com.co>
2020-02-24 16:18:26 -0500

    created firebase account (issue #2)

--- core/src/main/java/co/com/sofka/infraestructure/BdConnection.java
+++ core/src/main/java/co/com/sofka/infraestructure/BdConnection.java
@@ -1,4 +1,4 @@
-package co.com.sofka;
+package co.com.sofka.usecases;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
@@ -7,28 +7,23 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) throws IOException, ExecutionException, InterruptedException {
+public class BdConnection {
 
+    public static Firestore firebaseInstance() throws IOException {
+        //conexion con firebase
         FileInputStream serviceAccount = new FileInputStream("D:\\lisbey.urrea\\Documents\\SpringBootProject\\firebase-ddd-key\\event-soucing-demo-firebase-adminsdk-iqrs0-ff0b4b6e2c.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://events.firebaseio.com/")
                 .build();
 
         FirebaseApp.initializeApp(options);
 
         Firestore db = FirestoreClient.getFirestore();
 
-
+        return db;
     }
 }
